5362c2cbd5f5e6fb1a10b241d256216085a878b2
Mark Dewey <mark.t.dewey@gmail.com>
2018-08-27 10:48:39 -0400

    added basic css and html

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className="category">
+          <header>Category 1</header>
+          <section className="questions">Question 1</section>
+          <section className="questions">Question 2</section>
+          <section className="questions">Question 3</section>
+          <section className="questions">Question 4</section>
+        </section>
+        <section className="category">
+          <header>Category 2</header>
+          <section className="questions">Question 1</section>
+          <section className="questions">Question 2</section>
+          <section className="questions">Question 3</section>
+          <section className="questions">Question 4</section>
+        </section>
+        <section className="category">
+          <header>Category 3</header>
+          <section className="questions">Question 1</section>
+          <section className="questions">Question 2</section>
+          <section className="questions">Question 3</section>
+          <section className="questions">Question 4</section>
+        </section>
+        <section className="category">
+          <header>Category 4</header>
+          <section className="questions">Question 1</section>
+          <section className="questions">Question 2</section>
+          <section className="questions">Question 3</section>
+          <section className="questions">Question 4</section>
+        </section>
       </div>
     );
   }
